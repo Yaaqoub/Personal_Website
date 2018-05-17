cbd2038ec6b54ad3f586a67893879f96018aecbd
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2018-05-17 14:41:28 +0200

    change project structure and added sequelize

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors'),
+    express = require('express'),
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    logger = require('morgan'),
+    env = require('dotenv').load(),
+    passport = require('passport'),
+    session = require('express-session'),
+    bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//Exported models
+let models = require("./app/models");
 
-var app = express();
+//Sync Database
+if (process.env.NODE_ENV === 'production') {
+    models.sequelize.sync().then(function() {}).catch(function(err) {
+        throw new Error(err);
+    });
+}
+
+if (process.env.NODE_ENV === 'development') {
+    models.sequelize.sync(/*{force: true}*/).then(function() {
+         //require('./app/config/initial_data/index')(models);
+    }).catch(function(err) {
+        throw new Error(err);
+    });
+}
+
+
+let app = express();
+
+//Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// For Passport
+app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +48,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
